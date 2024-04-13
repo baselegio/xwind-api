9fdd6d72d65ac6406281ccc27031d9b7cba6c72e
kush-js <88463300+kush-js@users.noreply.github.com>
2024-04-13 16:58:43 -0400

    📝 chore(.gitignore): add .gitignore file to exclude node_modules directory from version control ✨ feat(index.js): create index.js file with basic express server setup and a root route handler 📝 chore(package.json): add package.json file with necessary dependencies for the xwind-api project

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+
+//handle posts
+app.use(bodyParser.json());
+
+/**
+ * Base Path
+ */
+app.get("/", (req, res, next) => {
+    return res.status(200).json({
+      message: "Hello from root!",
+    });
+});
+
+app.listen(port, () => console.log(`xwind api listening on port ${port}!`))
